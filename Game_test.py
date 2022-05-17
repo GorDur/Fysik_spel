52897299509ace5b8375b44eb75934d212b44673
GorDurg <gor.durg99@gmail.com>
2022-05-17 14:33:08 +0200

    spel v.3

--- Game_test.py
+++ Game_test.py
@@ -2,20 +2,23 @@ from secrets import choice
 import turtle       # to create shape with canvas
 import random     
 import time
+import math
 
 screen = turtle.Screen()    # defining screen 
 screen.bgcolor("black")     # screen color to black
 screen.title("Fysik spel")  # title of screen
 screen.tracer(0)            # turn off tracer
-counter = 0                 # 
+counter = 0                 
 wind_dir = [-1, 1]
 
 balls_shape = ["circle"]
 balls_colors = ["white", "orange", "green", "blue", "red"]
 balls = []          # array with all balls
+used_x_cords = []
+start_x = -290
 
-# creating as many balls as we need 
-for i in range(10):                  # creating 5 balls
+# creating balls 
+for i in range(10):                  # creating 1 balls
     balls.append(turtle.Turtle())   # appending them to the array
 
 # appending fetures to each ball
@@ -24,8 +27,11 @@ for b in balls:
     b.color(random.choice(balls_colors))    # balls get different colors
     b.penup()                               # to not leave tracks
     b.speed(0)                              # speed for showing movement
-    x = random.randint(-290, 290)           # random start position x
-    y = random.randint(200, 400)            # random start position y
+    x = start_x         
+    while x in used_x_cords and x <= 240:
+        x += 40
+    used_x_cords.append(x)
+    y = random.randint(100, 400)            # random start position y
     b.goto(x, y)                            # moving ball to that position
     b.dy = 0                                # initial velocity y
     b.dx = random.randint(-1, 1)            # initial velocity x
@@ -51,22 +57,21 @@ while True:
         b.setx(b.xcor() + b.dx)     # changing x cord
 
         # kollision med väggar
-        if b.xcor() > 300 or b.xcor() < -300:
+        if b.xcor() > 310 or b.xcor() < -310:
             b.dx *= -1
             b.da *= -1
 
         if b.ycor() < -300:         # friktion på marken
             b.sety(-300)
             b.dy *= -0.9
-            b.dx *= 0.9
             b.da *= -1
 
-        if counter % 500 == 0:      # circus air resitance
+        if counter % 300 == 0:      # circus air resistance
             b.dx *= air_res
             b.dy *= air_res
 
     # Wind force
-    if counter >= 5000:   # 5000ms = 5s
+    if counter >= 3000:   # 3000ms = 3s
         for b in balls:
             b.dx += 0.3 * random.choice(wind_dir)
             b.dy += 0.5 * random.choice(wind_dir)
@@ -77,8 +82,6 @@ while True:
     # det blir en totalt elastisk kollision mellan bollarna
     for i in range(0, len(balls)):
         for j in range(i+1, len(balls)):
-            if balls[i].distance(balls[j]) < 20:
-                #prev_dx, prev_dy = balls[i].dx, balls[i].dy
+            if balls[i].distance(balls[j]) < 25:
                 balls[i].dx, balls[j].dx = balls[j].dx, balls[i].dx
                 balls[i].dy, balls[j].dy = balls[j].dy, balls[i].dy
-                #balls[j].dx, balls[j],dy = prev_dx, prev_dys
